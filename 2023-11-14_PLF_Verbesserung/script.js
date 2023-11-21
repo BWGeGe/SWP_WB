60461bd3d940936261823fae45b3c9257b1581de
BWGeGe <BRZ210319@spengergasse.at>
2023-11-21 11:22:52 +0100

    made the js working

--- 2023-11-14_PLF_Verbesserung/script.js
+++ 2023-11-14_PLF_Verbesserung/script.js
@@ -5,17 +5,18 @@ h2Elements.forEach(h2 => {
     //for the clicked article
     let parents = e.target.parentElement;
     let kids = parents.children;
-    let kidsReverse = kids.reverse();
     let clickedHeadLine = kids[0];
     clickedHeadLine.opened = !clickedHeadLine.opened;
     //for all articles
-    h2Elements.forEach(parent => {
+    h2Elements.forEach(newsArticle => {
+      let parent = newsArticle.parentElement;
       if(parent != parents){
         let parOfNews = parent.children;
-        parOfNews[0].opened = !parOfNews[0].opened;
-        parOfNews[0].opened = false;
-        parOfNews[0].style.visibility = 'visible';
-        parOfNews[0].style.position = 'static';
+        let HeadLine = parOfNews[0];
+        HeadLine.opened = !HeadLine.opened;
+        HeadLine.opened = false;
+        HeadLine.style.visibility = 'visible';
+        HeadLine.style.position = 'static';
         for(let i = parOfNews.length - 1; i > 0; i--){ 
           parOfNews[i].style.visibility = 'hidden';
           parOfNews[i].style.position = 'absolute';
@@ -27,11 +28,19 @@ h2Elements.forEach(h2 => {
     if(clickedHeadLine.opened==true){
       clickedHeadLine.style.visibility = 'visible';
       clickedHeadLine.style.position = 'static';
+      parents.style.backgroundColor = 'white';
+      parents.style.color = 'black';  
       for(let i = kids.length - 1; i > 0; i--){
-        parents.removeChild(kids[i]);
-        parents.appendChild(kidsReverse[i]+"added");
-        kidsReverse[i].style.visibility = 'visible';
+        kids[i].style.visibility = 'visible';
         kids[i].style.position = 'static';
+      }
+    }
+    else if(clickedHeadLine == false){
+      parents.style.backgroundColor = '#5a8bc4';
+      parents.style.color = 'white';
+      for(let i = kids.length - 1; i > 0; i--){
+        kids[i].style.visibility = 'hidden';
+        kids[i].style.position = 'absolute';
       }
     }
   });
