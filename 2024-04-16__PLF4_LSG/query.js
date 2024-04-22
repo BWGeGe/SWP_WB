47e8174c6e8d5fcadaf54312b3795fa1ae190676
BWGeGe <brz210319@spengergasse.at>
2024-04-22 21:56:04 +0200

    hü; problem with prisma.findMany

---
+++ 2024-04-16__PLF4_LSG/query.js
@@ -0,0 +1,85 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
+let userNametemp;
+let trackNametemp;
+
+if (process.argv.length != 4) {
+    console.log('no specific action provided, exiting');
+    process.exit(1);
+}
+
+console.log(process.argv.at(-2));
+
+if(process.argv.at(-2) == 'u') {
+userNametemp = process.argv.at(-1).trim();
+if (!userNametemp) {
+    console.log('empty username provided, exiting');
+    process.exit(1);
+}
+}
+
+if(process.argv.at(-2) == 't') {
+trackNametemp = process.argv.at(-1).trim();
+if (!trackNametemp) {
+    console.log('empty trackname provided, exiting');
+    process.exit(1);
+}
+}
+
+const userName = userNametemp;
+const trackName = trackNametemp;
+
+async function getWatchlistNamesForUser(userName) {
+    return await prisma.watchlist.findMany({
+        select: {
+            id: true,
+            name: true,
+        },
+        where: {
+            benutzer: {
+                id: userName,
+            },
+        },
+    });
+}
+async function tracksFromWatchlist(id) {
+    return await prisma.track.findMany({
+        where: {
+            watchLists: {
+                some: { id: id },
+            },
+        },
+    });
+}
+
+async function watchlistfromTrackName(trackName) {
+    return await prisma.watchlist.findMany({
+        where: {
+            tracks: {
+                some: { name: trackName },
+            },
+        },
+    });  
+}
+
+async function main() {
+if(userName != undefined) {
+    const lists = await getWatchlistNamesForUser(userName);
+    for (let wl of lists) {
+        const tracks = await tracksFromWatchlist(wl.id);
+        console.log(
+            `${userName}'s Watchlist ${wl.name} ... ${tracks.length} tracks`
+        );
+        for (let t of tracks) {
+            console.log(`    ${t.name} by ${t.artist} (${t.duration} secs)`);
+        }
+    }
+}
+if(trackName != undefined) {
+    const onWatchlists = await watchlistfromTrackName(trackName);
+    for(let wl of onWatchlists) {
+        console.log(`Track ${trackName} is on Watchlist ${wl.name}`)
+    }
+}
+}
+main();
