a078987a50f0534288eb82e9d101644c173d917f
BWGeGe <brz210319@spengergasse.at>
2025-11-14 12:30:49 +0100

    Working seed.ts, checks if answer already in database

--- 2025-09-08_Prisma-recap/seed.ts
+++ 2025-09-08_Prisma-recap/seed.ts
@@ -1,137 +1,80 @@
 import { PrismaClient } from "./prisma/generated/client.ts";
 const prisma = new PrismaClient();
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 async function main() {
-    const delay = (ms: number) =>
-        new Promise((resolve) => setTimeout(resolve, ms));
+    ensureBasics();
+    const tokenGiven = await http(
+        "https://opentdb.com/api_token.php?command=request",
+    );
+    const tokenToUse = tokenGiven.token;
 
-    async function main() {
-        // Seed difficulties
-        await prisma.difficulty.createMany({
-            data: [
-                { level: "Easy" },
-                { level: "Medium" },
-                { level: "Hard" },
-            ],
-        });
+    // Fetch and seed questions for each category
+    const categories = await prisma.category.findMany();
+    for (const category of categories) {
+        const countData = await http(
+            `https://opentdb.com/api_count.php?category=${category.opentdb_id}`,
+        );
+        let questionsLeft = countData.category_question_count.total_question_count;
+        console.log(questionsLeft);
+        while (questionsLeft > 0) {
+            const maxQuestions = Math.min(questionsLeft, 50); // Limit to 50 questions per batch
+            questionsLeft -= maxQuestions;
+            const apiUrl =
+                `https://opentdb.com/api.php?amount=${maxQuestions}&token=${tokenToUse}&category=${category.opentdb_id}`;
 
-        // Seed types
-        await prisma.type.createMany({
-            data: [
-                { name: "Multiple Choice" },
-                { name: "True / False" },
-            ],
-        });
+            console.log(`Fetching questions for category: ${category.name}`);
+            const questionsData = await http(apiUrl);
 
-        // Seed categories
-        await prisma.category.createMany({
-            data: [
-                { name: "General Knowledge", opentdb_id: 9 },
-                { name: "Entertainment: Books", opentdb_id: 10 },
-                { name: "Entertainment: Film", opentdb_id: 11 },
-                { name: "Entertainment: Music", opentdb_id: 12 },
-                { name: "Entertainment: Musicals & Theatres", opentdb_id: 13 },
-                { name: "Entertainment: Television", opentdb_id: 14 },
-                { name: "Entertainment: Video Games", opentdb_id: 15 },
-                { name: "Entertainment: Board Games", opentdb_id: 16 },
-                { name: "Science & Nature", opentdb_id: 17 },
-                { name: "Science: Computers", opentdb_id: 18 },
-                { name: "Science: Mathematics", opentdb_id: 19 },
-                { name: "Mythology", opentdb_id: 20 },
-                { name: "Sports", opentdb_id: 21 },
-                { name: "Geography", opentdb_id: 22 },
-                { name: "History", opentdb_id: 23 },
-                { name: "Politics", opentdb_id: 24 },
-                { name: "Art", opentdb_id: 25 },
-                { name: "Celebrities", opentdb_id: 26 },
-                { name: "Animals", opentdb_id: 27 },
-                { name: "Vehicles", opentdb_id: 28 },
-                { name: "Entertainment: Comics", opentdb_id: 29 },
-                { name: "Science: Gadgets", opentdb_id: 30 },
-                { name: "Entertainment: Japanese Anime & Manga", opentdb_id: 31 },
-                { name: "Entertainment: Cartoon & Animations", opentdb_id: 32 },
-            ],
-        });
+            for (const question of questionsData.results) {
+                // Create the correct answer in the database
+                const correctAnswer = await createAnswer(question.correct_answer);
 
-        // Fetch and seed questions for each category
-        const categories = await prisma.category.findMany();
-        for (const category of categories) {
-            const tokenGiven = await http(
-                "https://opentdb.com/api_token.php?command=request",
-            );
-            const tokenToUse = tokenGiven.token;
-
-            const countData = await http(
-                `https://opentdb.com/api_count.php?category=${category.opentdb_id}`,
-            );
-            let questionsLeft =
-                countData.category_question_count.total_question_count;
-
-            while (questionsLeft > 0) {
-                const maxQuestions = Math.min(questionsLeft, 50); // Limit to 50 questions per batch
-                questionsLeft -= maxQuestions;
-                const apiUrl =
-                    `https://opentdb.com/api.php?amount=${maxQuestions}&token=${tokenToUse}&category=${category.opentdb_id}`;
-
-                console.log(`Fetching questions for category: ${category.name}`);
-                const questionsData = await http(apiUrl);
-
-                for (const question of questionsData.results) {
-                    // Create the correct answer in the database
-                    const correctAnswer = await prisma.answer.create({
-                        data: {
-                            answer: question.correct_answer,
-                        },
-                    });
-
-                    // Create the incorrect answers in the database
-                    const incorrectAnswers = await Promise.all(
-                        question.incorrect_answers.map(async (incorrect) => {
-                            return await prisma.answer.create({
-                                data: {
-                                    answer: incorrect,
-                                },
-                            });
-                        }),
-                    );
-
-                    // Create the question and connect it with the answers
-                    await prisma.question.create({
-                        data: {
-                            question: question.question,
-                            difficulty: { connect: { level: question.difficulty } },
-                            type: { connect: { name: question.type } },
-                            category: { connect: { id: category.id } },
-                            correct_answer: {
-                                connect: { id: correctAnswer.id },
-                            },
-                            incorrect_answers: {
-                                connect: incorrectAnswers.map((ans) => ({ id: ans.id })),
-                            },
-                        },
-                    });
-                    await delay(5000); // Wait 5 seconds before the next API call
-                }
-
-                console.log(
-                    `Seeded ${questionsData.results.length} questions for ${category.name}`,
+                // Create the incorrect answers in the database
+                const incorrectAnswers = await Promise.all(
+                    question.incorrect_answers.map(async (incorrect) => {
+                                return await createAnswer(incorrect);
+                    }),
                 );
+
+                // Create the question and connect it with the answers
+                await prisma.question.create({
+                    data: {
+                        question: question.question,
+                        difficulty: { connect: { level: question.difficulty } },
+                        type: { connect: { name: question.type } },
+                        category: { connect: { id: category.id } },
+                        correct_answer: {
+                            connect: { id: correctAnswer.id },
+                        },
+                        incorrect_answers: {
+                            connect: incorrectAnswers.map((ans) => ({ id: ans.id })),
+                        },
+                    },
+                });
+                console.log(`1 question added to: ${category.name}`);
             }
 
-            console.log("Seeding completed!");
+            await delay(5000); // Wait 5 seconds before the next API call
+            console.log(
+                `Seeded ${questionsData.results.length} questions for ${category.name}`,
+            );
         }
 
-        main()
-            .catch((e) => {
-                console.error(e);
-                process.exit(1);
-            })
-            .finally(async () => {
-                await prisma.$disconnect();
-            });
+        console.log("Seeding completed!");
     }
 
-    console.log("Seed data inserted");
+    main()
+        .catch((e) => {
+            console.error(e);
+            process.exit(1);
+        })
+        .finally(async () => {
+            await prisma.$disconnect();
+        });
 }
+
+console.log("Seed data inserted");
 main();
 
 export async function http(
@@ -140,4 +83,51 @@ export async function http(
     const response = await fetch(request);
     const body = await response.json();
     return body;
+}
+
+export async function ensureBasics() {
+    //only checking category as they are if it is seeded the others will be too
+    const existingCategories = await prisma.category.findMany();
+    if (existingCategories.length > 0) {
+        return;
+    }
+    await prisma.difficulty.createMany({
+        data: [
+            { level: "easy" },
+            { level: "medium" },
+            { level: "hard" },
+        ],
+    });
+
+    // Seed types
+    await prisma.type.createMany({
+        data: [
+            { name: "multiple" },
+            { name: "boolean" },
+        ],
+    });
+
+    const categoriesApi = await http("https://opentdb.com/api_category.php");
+    // Seed categories
+    await prisma.category.createMany({
+        data: categoriesApi.trivia_categories.map((cat) => ({
+            opentdb_id: cat.id,
+            name: cat.name,
+        })),
+    });
+}
+
+export async function createAnswer(answerText){
+    const thisAnswer = await prisma.answer.findFirst({
+        where: {answer: answerText},
+    });
+    if(thisAnswer != null ){
+        return thisAnswer;
+    }
+ const answerCreated = await prisma.answer.create({
+                    data: {
+                        answer: answerText,
+                    },
+                });
+return answerCreated;
 }
